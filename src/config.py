0a1e7a9718a94b96c059530a4ad36882944a495c
Kwanghyun On <kwanghyun.on@gmail.com>
2022-02-16 21:46:18 +0900

    Add new arguments: --resume, --reset

--- src/config.py
+++ src/config.py
@@ -4,35 +4,56 @@ import yaml
 
 
 class Config:
-    def __init__(self, cfg_path):
+    def __init__(self, cfg_path, resume, reset):
         self.cfg_path = cfg_path
+        self.resume = resume
+        self.reset = reset
         with open(self.cfg_path) as f:
             self.cfg = yaml.load(f, Loader=yaml.FullLoader)
         self._parse_config()
         self._set_dirs()
 
     def _parse_config(self):
-        self.model_name = self.cfg["model"]["name"]
-        self.model_kwargs = self.cfg["model"]["kwargs"]
+        self.model_name = self._get_value("model", "name")
+        self.model_kwargs = self._get_value("model", "kwargs")
 
-        self.dataset_name = self.cfg["data"]["dataset"]["name"]
-        self.dataset_kwargs = self.cfg["data"]["dataset"]["kwargs"]
-        self.loader_kwargs = self.cfg["data"]["loader"]["kwargs"]
+        self.transform_name = self._get_value("data", "transform", "name")
+        self.transform_kwargs = self._get_value("data", "transform", "kwargs")
 
-        self.optim_name = self.cfg["optim"]["name"]
-        self.optim_kwargs = self.cfg["optim"]["kwargs"]
+        self.dataset_name = self._get_value("data", "dataset", "name")
+        self.dataset_kwargs = self._get_value("data", "dataset", "kwargs")
+        self.loader_kwargs = self._get_value("data", "loader", "kwargs")
 
-        self.trainer_name = self.cfg["trainer"]["name"]
-        self.trainer_kwargs = self.cfg["trainer"]["kwargs"]
+        self.optim_name = self._get_value("optim", "name")
+        self.optim_kwargs = self._get_value("optim", "kwargs")
 
-        self.epochs = self.cfg["train"]["epochs"]
+        self.trainer_name = self._get_value("trainer", "name")
+        self.trainer_kwargs = self._get_value("trainer", "kwargs")
+
+        self.epochs = self._get_value("train", "epochs")
 
     def _set_dirs(self):
         self.exp_path = os.path.join(
-            self.cfg["exp"]["root"],
-            self.cfg["exp"]["name"],
-            self.cfg["exp"]["ablation"],
+            self._get_value("exp", "root"),
+            self._get_value("exp", "name"),
+            self._get_value("exp", "ablation"),
         )
+        ckpt_path = os.path.join(self.exp_path, "ckpts", "latest.ckpt")
+        if self.reset:
+            shutil.rmtree(self.exp_path, ignore_errors=True)
+        elif os.path.exists(ckpt_path) and not self.resume:
+            raise Exception(
+                "Checkpoint found. Add --resume to resume the experiment or --reset to remove the existing training results."
+            )
         os.makedirs(self.exp_path, exist_ok=True)
         self.trainer_kwargs["exp_path"] = self.exp_path
         shutil.copyfile(self.cfg_path, os.path.join(self.exp_path, "config.yaml"))
+
+    def _get_value(self, *keys):
+        d = self.cfg
+        for k in list(keys):
+            if k not in d:
+                return None
+            else:
+                d = d[k]
+        return d
--- src/train.py
+++ src/train.py
@@ -8,18 +8,28 @@ from config import Config
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--cfg", type=str, required=True)
+parser.add_argument("--config", type=str, required=True)
 parser.add_argument("--resume", action="store_true")
+parser.add_argument("--reset", action="store_true")
 args = parser.parse_args()
-cfg = Config(args.cfg)
+cfg = Config(args.config, args.resume, args.reset)
 
 model = get_model(cfg.model_name, cfg.model_kwargs)
 train_loader, eval_loader = get_dataloader(
-    cfg.dataset_name, cfg.dataset_kwargs, cfg.loader_kwargs
+    cfg.transform_name,
+    cfg.transform_kwargs,
+    cfg.dataset_name,
+    cfg.dataset_kwargs,
+    cfg.loader_kwargs,
 )
 optimizer = get_optimizer(cfg.optim_name, cfg.optim_kwargs, model.parameters())
 trainer = get_trainer(
-    cfg.trainer_name, cfg.trainer_kwargs, model, optimizer, train_loader, eval_loader
+    cfg.trainer_name,
+    cfg.trainer_kwargs,
+    model,
+    optimizer,
+    train_loader,
+    eval_loader,
 )
 
 trainer.run(cfg.epochs, args.resume)
