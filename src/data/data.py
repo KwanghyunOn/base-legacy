706e8165d7d46cdb99b5c560e9fa0358ec6222a6
Kwanghyun On <kwanghyun.on@gmail.com>
2022-02-18 21:53:10 +0900

    Support DistributedDataParallel

--- src/data/data.py
+++ src/data/data.py
@@ -1,5 +1,6 @@
 from importlib import import_module
 from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
 
 
 def get_transform(transform_name, **transform_kwargs):
@@ -17,17 +18,46 @@ def get_dataset(dataset_name, **dataset_kwargs):
 
 
 def get_dataloader(transform_name, transform_kwargs, dataset_name, dataset_kwargs, loader_kwargs):
-    train_transform = get_transform(transform_name, **transform_kwargs, train=True)
-    eval_transform = get_transform(transform_name, **transform_kwargs, train=False)
-    train_dataset = get_dataset(dataset_name, **dataset_kwargs, transform=train_transform, train=True)
-    eval_dataset = get_dataset(dataset_name, **dataset_kwargs, transform=eval_transform, train=False)
-    
     if "eval_kwargs" in loader_kwargs:
         eval_kwargs = loader_kwargs.pop("eval_kwargs")
-        eval_loader_kwargs = loader_kwargs.copy()
-        eval_loader_kwargs.update(eval_kwargs)
+        loader_eval_kwargs = loader_kwargs.copy()
+        loader_eval_kwargs.update(eval_kwargs)
     else:
-        eval_loader_kwargs = loader_kwargs
-    train_loader = DataLoader(train_dataset, **loader_kwargs, shuffle=True)
-    eval_loader = DataLoader(eval_dataset, **eval_loader_kwargs, shuffle=False)
-    return train_loader, eval_loader
+        loader_eval_kwargs = loader_kwargs
+
+    if transform_name:
+        transform_train = get_transform(transform_name, **transform_kwargs, train=True)
+        transform_eval = get_transform(transform_name, **transform_kwargs, train=False)
+    else:
+        transform_train = None
+        transform_eval = None
+
+    dataset_train = get_dataset(dataset_name, **dataset_kwargs, transform=transform_train, train=True)
+    dataset_eval = get_dataset(dataset_name, **dataset_kwargs, transform=transform_eval, train=False)
+    loader_train = DataLoader(dataset_train, **loader_kwargs, shuffle=True)
+    loader_eval = DataLoader(dataset_eval, **loader_eval_kwargs, shuffle=False)
+    return loader_train, loader_eval
+
+
+def get_dataloader_ddp(transform_name, transform_kwargs, dataset_name, dataset_kwargs, loader_kwargs):
+    if "eval_kwargs" in loader_kwargs:
+        eval_kwargs = loader_kwargs.pop("eval_kwargs")
+        loader_eval_kwargs = loader_kwargs.copy()
+        loader_eval_kwargs.update(eval_kwargs)
+    else:
+        loader_eval_kwargs = loader_kwargs
+
+    if transform_name:
+        transform_train = get_transform(transform_name, **transform_kwargs, train=True)
+        transform_eval = get_transform(transform_name, **transform_kwargs, train=False)
+    else:
+        transform_train = None
+        transform_eval = None
+
+    dataset_train = get_dataset(dataset_name, **dataset_kwargs, transform=transform_train, train=True)
+    dataset_eval = get_dataset(dataset_name, **dataset_kwargs, transform=transform_eval, train=False)
+    sampler_train = DistributedSampler(dataset_train, shuffle=True)
+    sampler_eval = DistributedSampler(dataset_eval, shuffle=False)
+    loader_train = DataLoader(dataset_train, **loader_kwargs, sampler=sampler_train, pin_memory=True)
+    loader_eval = DataLoader(dataset_eval, **loader_eval_kwargs, sampler=sampler_eval, pin_memory=True)
+    return loader_train, loader_eval
---
+++ src/launch.py
@@ -0,0 +1,23 @@
+import sys
+import argparse
+import subprocess
+import torch
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--world_size", type=int, default=torch.cuda.device_count())
+args, unknown = parser.parse_known_args()
+
+args_launch = ["python", "-m", "torch.distributed.launch",
+               "--nproc_per_node", str(args.world_size),
+               "train.py", *unknown]
+p = subprocess.Popen(args_launch, stdout=sys.stdout, stderr=sys.stderr)
+
+try:
+    p.wait()
+except KeyboardInterrupt:
+    try:
+        p.terminate()
+    except OSError:
+        pass
+    p.wait()
